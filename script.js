6cbcc52d82e6d9270e7fdcf8c8c36b4298c1584a
KrishAryan <krisharyan.36@gmail.com>
2021-10-21 14:14:08 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let myBlock;
 document.addEventListener("DOMContentLoaded",function(){
-    console.log("ready")
     myBlock = document.createElement("div")
     myBlock.textContent = "Hello world";
+    myBlock.style.width="100px";
+    myBlock.style.height="100px";
+    myBlock.style.backgroundColor="red";
+    myBlock.style.color="white";
+    myBlock.style.lineHeight="100px";
+    myBlock.style.textAlign="center";
+    myBlock.style.postition="absolute";
+    myBlock.style.top="100px";
+    myBlock.style.left="150px";
     document.body.appendChild(myBlock);
 })
